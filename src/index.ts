c55662c5cbdae8fa2deb82f0662cfeeb57050749
t.momose <hyakkun@gmail.com>
2025-06-19 11:17:32 +0900

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
 import { getInput, setOutput } from "@actions/core";
-import fetch from "node-fetch";
+import fetch, { type Response as NodeFetchResponse } from "node-fetch";
+
+function main() {
+	const org = getInput("org");
+	const packageType = getInput("packageType");
+	const packageName = getInput("packageName");
+	const githubToken = getInput("githubToken");
+	const versionPattern = getInput("versionPattern");
+	const version = getLatestVersion({
+		githubToken,
+		org,
+		packageType,
+		packageName,
+		versionPattern,
+	});
+	version
+		.then((v) => {
+			if (!v) {
+				throw new Error(
+					`Could not find version for package ${packageName} in org ${org}`,
+				);
+			}
+			console.log(`version: ${v}`);
+			setOutput("version", v);
+		})
+		.catch((error) => {
+			console.error(error);
+			process.exit(1);
+		});
+}
 
 async function getLatestVersion({
 	githubToken,
@@ -14,23 +43,13 @@ async function getLatestVersion({
 	packageType: string;
 	packageName: string;
 }): Promise<string | undefined> {
-	// https://docs.github.com/en/rest/packages/packages?apiVersion=2022-11-28#list-package-versions-for-a-package-owned-by-an-organization
-	let authHeader = {};
-	if (githubToken) {
-		authHeader = {
-			Authorization: `Bearer ${githubToken}`,
-		};
-	}
-	const url = `https://api.github.com/orgs/${org}/packages/${packageType}/${packageName}/versions`;
-	const options = {
-		headers: {
-			...authHeader,
-			Accept: "application/vnd.github+json",
-			"X-GitHub-Api-Version": "2022-11-28",
-		},
-	};
 	const versionRegex = versionPattern && new RegExp(versionPattern);
-	return fetch(url, options)
+	return getVersions({
+		githubToken,
+		org,
+		packageType,
+		packageName,
+	})
 		.then((response) =>
 			response.json().then((data) => ({
 				ok: response.ok,
@@ -51,28 +70,33 @@ async function getLatestVersion({
 		});
 }
 
-function fetchGithubData() {
-	const org = getInput("org");
-	const packageType = getInput("packageType");
-	const packageName = getInput("packageName");
-	const githubToken = getInput("githubToken");
-	const versionPattern = getInput("versionPattern");
-	const version = getLatestVersion({
-		githubToken,
-		org,
-		packageType,
-		packageName,
-		versionPattern,
+async function getVersions({
+	githubToken,
+	org,
+	packageType,
+	packageName,
+}: {
+	githubToken: string | undefined;
+	org: string;
+	packageType: string;
+	packageName: string;
+}): Promise<NodeFetchResponse> {
+	// https://docs.github.com/en/rest/packages/packages?apiVersion=2022-11-28#list-package-versions-for-a-package-owned-by-an-organization
+	let authHeader = {};
+	if (githubToken) {
+		authHeader = {
+			Authorization: `Bearer ${githubToken}`,
+		};
+	}
+	const url = `https://api.github.com/orgs/${org}/packages/${packageType}/${packageName}/versions`;
+	console.log(`fetching ${url}`);
+	return fetch(url, {
+		headers: {
+			...authHeader,
+			Accept: "application/vnd.github+json",
+			"X-GitHub-Api-Version": "2022-11-28",
+		},
 	});
-	version
-		.then((v) => {
-			console.log(`version: ${v}`);
-			setOutput("version", v);
-		})
-		.catch((error) => {
-			console.error(error);
-			process.exit(1);
-		});
 }
 
-fetchGithubData();
+main();
